e79c4249b6145824b54aa8bcf249b1f711e9076e
GokulRSG <gokulnath2718@gmail.com>
2023-11-09 14:29:47 +0530

    JS Warmup

---
+++ GUVI WarmUp - 6.js
@@ -0,0 +1,18 @@
+/* Write a function that converts hours into seconds. */
+
+function hourToSeconds(hours) {
+	///Your code Starts here 
+	
+	
+	return 60 * 60 * hours;
+	
+	
+	///Your code Ends here
+}
+/*
+Lines For TestCase
+
+hourToSeconds(24);
+hourToSeconds(10);
+hourToSeconds(2);
+*/
